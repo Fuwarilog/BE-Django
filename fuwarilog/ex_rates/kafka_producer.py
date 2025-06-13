a4174e83b8db73abca6d978ca6550840427ab5ff
afflogy <dlatnqls921@naver.com>
2025-06-13 14:53:02 +0900

    2025-06-13 kafka producer를 통한 예측 데이터 db저장 로직 구현

--- fuwarilog/ex_rates/kafka_producer.py
+++ fuwarilog/ex_rates/kafka_producer.py
@@ -1,11 +1,11 @@
 from kafka import KafkaProducer
+from numpyencoder import NumpyEncoder
 import json
 
-producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',
-    value_serializer=lambda m: json.dumps(m).encode('utf-8'),
-)
-
 def send_prediction(topic, message: dict):
+    producer = KafkaProducer(
+        bootstrap_servers='localhost:9092',
+        value_serializer=lambda m: json.dumps(m, cls=NumpyEncoder).encode('utf-8'),
+    )
     producer.send(topic, message)
     producer.flush()
